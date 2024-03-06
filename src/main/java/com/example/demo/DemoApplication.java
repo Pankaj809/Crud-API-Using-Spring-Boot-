d391b7790e85df52d78533b3aff174f3403251aa
Pankaj Adhikari <pankajadhikari809@gmail.com>
2024-03-06 22:30:51 +0545

    string boot application

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 @SpringBootApplication
-
+//it is used to represent that it is spring boot application
 public class DemoApplication {
 
 	public static void main(String[] args) {
